dd3eae7fa76e432d8585698ff9f23e167c2ca26e
Faria Sultana Monia <faria.cse.32@gmail.com>
2024-10-04 15:10:40 +0600

    Create Task-8-area of a circel.cpp

---
+++ Task-8-area of a circel.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int R;
+    float Area;
+    printf("Enter Radius");
+    scanf("%d",&R);
+    
+    Area=3.14159*R*R;
+    
+     printf("Area=%f",Area);
+     
+    return 0;
+}
